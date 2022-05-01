2a6d3819ae3969e73476f450406a70b7fbd314fd
Guppy <15827112306@163.com>
2022-05-01 16:47:30 +0300

    第一版

--- 1.js
+++ 1.js
@@ -1,64 +1,85 @@
-
-// 拿到ID是可以做全局变量的，既然是全局变量，那么久跟Object一样可以调用方法
-// canvas.onclick = (e)=>{
-//     console.log(e.clientX)
-//     console.log(e.clientY)
-//     // console.log调试大法
-// }
-
-// onmousemove事件表示鼠标移动触发
-// 下面代码使用JS去操作DOM，因为跨线程，所以性能不好，画出来的会卡顿，所以才需要canvas
-// canvas.onmousemove = function (e) {
-//     console.log(e.clientX)
-//     console.log(e.clientY)
-//     // console.log调试大法
-//     let div = document.createElement('div')
-//     div.style.position = 'absolute'
-//     div.style.left = e.clientX + 'px'
-//     div.style.top = e.clientY + 'px'
-//     // div.style.border = '1px solid red'
-//     div.style.height = '6px'
-//     div.style.width = '6px'
-//     div.style.marginLeft = '-3px'
-//     div.style.marginTop = '-3PX'
-//     div.style.borderRadius = '50%'
-//     div.style.backgroundColor = 'black'
-//     // 用createElement创建后的div只是在内存中，要想出现在页面用要用appendChild添加到canvas元素中
-//     canvas.appendChild(div)
-// }
-
-// 用canvas划线（canvas默认是一个inline元素，可以从板块的computed中看display），canvas是类似与img的元素，不是div
-// 与img元素一样，canvas也可以直接写宽高，只不过与img一样，会被css覆盖
-// 用canvas代码，js不会新建DOM结点，使用显卡和cpU直接算出来
+// 解决画得太快连不成线的问题
 let canvas = document.getElementById("canvas");
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
 let ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.strokeStyle = 'none';
+ctx.lineWidth = 10;
+// 表示节点的连接是圆的（节点即点与点之间）
+// ctx.lineJoin = "round"
+// lineCap可以解决划线锯齿状问题
+ctx.lineCap = "round"
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+
+
+// ctx.lineTo(0, 500);
+// 把最后一个点与起始点连起来
+// ctx.closePath();
+// stroke用于描边，用ctx.fill()就是实现填充效果
 
 let painting = false
+let last
 
-ctx.fillStyle = "black";
-ctx.strokeStyle = 'none'
+var isTouchDevice = 'ontouchstart' in document.documentElement
 
-canvas.onmousedown = () => {
-    painting = true
-}
-canvas.onmousemove = (e) => {
-    if (painting === true) {
-        // console.log(e.clientX)
-        // console.log(e.clientY)
-        // ctx.fillRect(e.clientX - 5, e.clientY - 5, 10, 10)
-        ctx.beginPath();
-        ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI)
-        ctx.stroke()
-        ctx.fill()
-    } else {
-        console.log("什么都不做")
+
+if (isTouchDevice) {
+    canvas.ontouchstart = (e) => {
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        last = [x, y]
     }
+    canvas.ontouchmove = (e) => {
+        // console.log(e)
+        let x = e.touches[0].clientX
+        let y = e.touches[0].clientY
+        drawLine(last[0], last[1], x, y)
+        last = [x, y]
+        // ctx.beginPath();
+        // ctx.arc(x, y, 10, 0, 2 * Math.PI)
+        // ctx.stroke()
+        // ctx.fill()
 
+    }
+} else {
+    canvas.onmousedown = (e) => {
+        painting = true
+        last = [e.clintX, e.clientY]
+    }
+    canvas.onmousemove = (e) => {
+        if (painting == true) {
+            drawLine(last[0], last[1], e.clientX, e.clientY)
+            last = [e.clientX, e.clientY]
+        }
+    }
+    console.log("根据’ontouchstart‘ in document.documentElement判断出该设备不支持触摸屏 ")
+    canvas.onmouseup = () => {
+        painting = false
+    }
 }
-canvas.onmouseup = () => {
-    painting = false
-}
+
+// 移动端画线时，页面也跟着滑动解决办法：在style.css里面加入以下样式：body{ overflow: hidden; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
